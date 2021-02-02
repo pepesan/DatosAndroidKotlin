c9f6c05b5fd36c746068df21767b3e145bb4d373
David <dvaquero@bilbomatica.es>
2021-02-02 16:39:04 +0100

    mejora del ejemplo de nuevas preferencias compartidas con fragmento

--- app/src/main/java/com/cursosdedesarrollo/datosandroidkotlin/CompatSettingsActivity.kt
+++ app/src/main/java/com/cursosdedesarrollo/datosandroidkotlin/CompatSettingsActivity.kt
@@ -1,21 +1,42 @@
 package com.cursosdedesarrollo.datosandroidkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import java.net.MalformedURLException
 
-class CompatSettingsActivity : AppCompatActivity() {
+class CompatSettingsActivity : AppCompatActivity(), SharedPreferences.OnSharedPreferenceChangeListener  {
     class MySettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.preferences_jetpack, rootKey)
         }
     }
+
+    var preferencesChanged = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_compat_settings)
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .registerOnSharedPreferenceChangeListener(this);
         supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.settings_fragment, MySettingsFragment())
                 .commit()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        PreferenceManager.getDefaultSharedPreferences(this)
+            .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreference: SharedPreferences?, key: String?) {
+        Log.d("app","Cambio en preferencias")
+        //sólo si es de tipo String
+        Log.d("App","Preference: "+sharedPreference?.getString(key,"").toString())
     }
 }
